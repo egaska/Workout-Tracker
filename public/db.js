0f37dd0b8070ad5d0eb5cb4b0be3f4894897c233
Emily Gaska <emilygaska@gmail.com>
2020-06-15 17:25:08 -0500

    Added HTML Routes

--- public/db.js
+++ public/db.js
@@ -1,27 +1,29 @@
 let db;
-// create a new db request for a "workout" database.
-const request = indexedDB.open("workout", 1);
 
-request.onupgradeneeded = function(event) {
-    // create object store called "pending" and set autoIncrement to true
-   const db = event.target.result;
-   db.createObjectStore("pending", { autoIncrement: true });
- };
+// We request a database instance
+const request = window.indexedDB.open("workout", 1);
 
- request.onsuccess = function(event) {
-    db = event.target.result;
-  
-    // check if app is online before reading from db
-    if (navigator.onLine) {
-      checkDatabase();
-    }
-  };
+// Create an object store inside the onupgradeneeded method.
+request.onupgradeneeded = ({ target }) => {
+  const db = target.result;
+  const objectStore = db.createObjectStore("workout", { autoIncrement: true });
+};
 
-  request.onerror = function(event) {
+// On success console the result.
+request.onsuccess = (event) => {
+  console.log(request.result);
+
+  // check if app is online before reading from db
+  if (navigator.onLine) {
+    checkDatabase();
+  }
+};
+
+request.onerror = function(event) {
     console.log("Woops! " + event.target.errorCode);
   };
   
-  function saveRecord(record) {
+function saveRecord(record) {
     // create a transaction on the pending db with readwrite access
     const transaction = db.transaction(["pending"], "readwrite");
   
@@ -31,35 +33,41 @@ request.onupgradeneeded = function(event) {
     // add record to your store with add method.
     store.add(record);
   }
-
-  function checkDatabase() {
+  
+  
+function checkDatabase() {
     // open a transaction on your pending db
     const transaction = db.transaction(["pending"], "readwrite");
     // access your pending object store
     const store = transaction.objectStore("pending");
     // get all records from store and set to a variable
-    const getLastWorkout = store.getLastWorkout();
+    const getAll = store.getAll();
   
-  getLastWorkout.onsuccess = function() {
-    if (getLastWorkout.result.length > 0) {
-      fetch("/api/workouts", {
-        method: "POST",
-        body: JSON.stringify(getLastWorkout.result),
-        headers: {
-          Accept: "application/json, text/plain, */*",
-          "Content-Type": "application/json"
-        }
-      })
-      .then(response => response.json())
-      .then(() => {
-        // if successful, open a transaction on your pending db
-        const transaction = db.transaction(["pending"], "readwrite");
-
-        // access your pending object store
-        const store = transaction.objectStore("pending");
-
-        // clear all items in your store
-        store.clear();
-      });
-    }
-}
+    getAll.onsuccess = function() {
+      if (getAll.result.length > 0) {
+        fetch("/api/transaction/bulk", {
+          method: "POST",
+          body: JSON.stringify(getAll.result),
+          headers: {
+            Accept: "application/json, text/plain, */*",
+            "Content-Type": "application/json"
+          }
+        })
+        .then(response => response.json())
+        .then(() => {
+          // if successful, open a transaction on your pending db
+          const transaction = db.transaction(["pending"], "readwrite");
+  
+          // access your pending object store
+          const store = transaction.objectStore("pending");
+  
+          // clear all items in your store
+          store.clear();
+        });
+      }
+    };
+  }
+  
+  // listen for app coming back online
+  window.addEventListener("online", checkDatabase);
+  
--- server.js
+++ server.js
@@ -1,27 +1,27 @@
+
 const express = require("express");
-const mongoose = require("mongoose");
 const logger = require("morgan");
+const mongoose = require("mongoose");
+
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
 app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(require("./public/api"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
-
-//
